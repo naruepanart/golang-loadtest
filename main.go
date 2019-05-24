6e199c85ddc14ea55866bc20e3a89fe040708335
Naruepanart Siangsanan <naruepanart@users.noreply.github.com>
2019-05-25 00:46:45 +0700

    update

--- main.go
+++ main.go
@@ -4,21 +4,41 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-
+	"github.com/labstack/echo/middleware"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func getUsers(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!\n")
+type Person struct {
+	ID bson.ObjectId `bson:"_id,omitempty"`
+	username string `bson:"username"`
+	password string `bson:"password"`
 }
 
-func main() {
-	// Echo instance
-	e := echo.New()
+func getUsers(c echo.Context) error {
+	return c.String(http.StatusOK, "getUsers\n")
+}
+func createUsers(c echo.Context) error {
+	return nil
+}
+func deleteUser(c echo.Context) error {
+	return c.String(http.StatusOK, "deleteUser\n")
+}
 
-	// Middleware
+const (
+	mongoURL = "mongodb+srv://guload:mNW4RK1skR2yTdT5@guload-singapore-w5sv8.gcp.mongodb.net/main?retryWrites=true"
+)
+
+func main() {
+	e := echo.New()
+	
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+	}))
 
 	// Route => handler
 	e.GET("/", getUsers)
+	e.POST("/", createUsers)
+	e.DELETE("/:id", deleteUser)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
